f9e5ea3514445b3e3d75e00ea4dd8d1e023a3d41
Xavi <xquesadas@uoc.edu>
2023-11-23 21:40:25 +0100

    Añadido apartado resolucion - Quitado calidad

--- Assets/Scripts/CodigoPantallaCompleta.cs
+++ Assets/Scripts/CodigoPantallaCompleta.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class CodigoPantallaCompleta : MonoBehaviour
 {
     
     public Toggle toggle;
+    public TMP_Dropdown resolucionesDropDown;
+    Resolution[] resoluciones;
 
     public void Start()
     {
@@ -18,6 +21,8 @@ public class CodigoPantallaCompleta : MonoBehaviour
         {
             toggle.isOn = false;
         }
+
+        RevisarResolucion();
     }
 
     void Update()
@@ -28,6 +33,40 @@ public class CodigoPantallaCompleta : MonoBehaviour
     public void ActivarPantallaCompleta(bool pantallaCompleta)
     {
         Screen.fullScreen = pantallaCompleta;
+    }
+
+    public void RevisarResolucion()
+    {
+        resoluciones = Screen.resolutions;
+        resolucionesDropDown.ClearOptions();
+        List<string> opciones = new List<string>();
+        int resolucionActual = 0;
+
+        for (int i = 0; i < resoluciones.Length; i++)
+        {
+            string opcion = resoluciones[i].width + " x " + resoluciones[i].height;
+            opciones.Add(opcion);
+
+            if (Screen.fullScreen && resoluciones[i].width == Screen.currentResolution.width && resoluciones[i].height == Screen.currentResolution.height)
+            {
+                resolucionActual = i;
+            }
+        }
+
+        resolucionesDropDown.AddOptions(opciones);
+        resolucionesDropDown.value = resolucionActual;
+        resolucionesDropDown.RefreshShownValue();
+
+        resolucionesDropDown.value = PlayerPrefs.GetInt("numeroResolucion", 0);
+    }
+
+    public void CambiarResolucion(int indiceResolucion)
+    {
+
+        PlayerPrefs.SetInt("numeroResolucion", resolucionesDropDown.value);
+
+        Resolution resolucion = resoluciones[indiceResolucion];
+        Screen.SetResolution(resolucion.width, resolucion.height, Screen.fullScreen);
     }
 
 }
